e82b90dfa92692e4151746a4af675e14c96e5007
MgKyaw <mgkyaw20112@gmail.com>
2025-10-07 22:01:49 +0700

    feat: Add new interface IHostedService

---
+++ PostNotificationService/IHostedService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace PostNotificationService
+{
+    public interface IHostedService
+    {
+        Task StartAsync(CancellationToken cancellationToken);
+        Task StopAsync(CancellationToken cancellationToken);
+    }
+}
